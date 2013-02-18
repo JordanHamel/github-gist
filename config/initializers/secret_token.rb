36d49160457fb0c7267390245567a7a5720e32bb
Jordan and Vincent <jordan+vincent@appacademy.io>
2013-02-18 10:51:38 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GistProject::Application.config.secret_token = '562083642f2586e4ebd889e19d28c0babc8eb44667ba0ce22e2cca8022ea389d08af4c449cd4ea65a9d9ec0c531d444bf1372582f65d0bd8cc63f6370c7df158'
